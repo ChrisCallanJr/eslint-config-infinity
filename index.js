657789334a2a8d5bc6090cc26e5c204072bf3f0a
Ethan Martin <Martin.Ethan@pennmutual.com>
2017-09-22 13:34:16 -0400

    Bring over rules from Insight. Specify peer dependency on the "standard" style guide.

---
+++ index.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "extends": "standard",
+  "rules": {
+    "comma-dangle": ["error", "always-multiline"],
+    "consistent-this": ["error", "self"],
+    "linebreak-style": ["error", "unix"],
+    "newline-after-var": ["error", "always"],
+    "no-else-return": "error",
+    "no-implicit-coercion": "error",
+    "no-lonely-if": "error",
+    "no-shadow": "error",
+    "no-useless-return": "error",
+    "object-curly-spacing": ["error", "never"],
+    "quote-props": ["error", "consistent"],
+    "space-before-function-paren": ["error", "never"],
+    "vars-on-top": "warn"
+  }
+}
